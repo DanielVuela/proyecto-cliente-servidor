77aa49cf2acc6e4ecb760b7d3f2d15c7475ef8a4
AlexxLopez1 <mlopez01614@ufide.ac.cr>
2024-12-09 19:09:06 -0600

    Revert "Carrito de compras"
    This reverts commit 141b3a5150cda586a7996b4e911a470ecf2daf1b.

--- Client/js/main.js
+++ Client/js/main.js
@@ -1,30 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Página cargada correctamente.");
-    updateCartCount();
+console.log("Página cargada correctamente.");
 
-    const addToCartButtons = document.querySelectorAll(".add-to-cart");
-    addToCartButtons.forEach(button => {
-        button.addEventListener("click", function(event) {
-            event.preventDefault();
-            const productCard = this.closest(".card");
-            const product = {
-                title: productCard.querySelector(".card-title").innerText,
-                price: productCard.querySelector(".card-text").innerText,
-                image: productCard.querySelector("img").src
-            };
-            addToCart(product);
-        });
-    });
-});
-
-function addToCart(product) {
-    let cart = JSON.parse(localStorage.getItem("cart")) || [];
-    cart.push(product);
-    localStorage.setItem("cart", JSON.stringify(cart));
-    updateCartCount();
-}
-
-function updateCartCount() {
-    const cart = JSON.parse(localStorage.getItem("cart")) || [];
-    document.getElementById("cart-count").innerText = cart.length;
-}
+<script src="js/main.js"></script>
