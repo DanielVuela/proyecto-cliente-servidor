f2d3e79cfcffe8e59111350b546e786785b7e500
Jurgem ramirez <42251447+JurgemRamirez@users.noreply.github.com>
2024-12-17 17:25:00 -0600

    corregi dato stock

--- Client/js/get_product.js
+++ Client/js/get_product.js
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         <h5 class="card-title">${product.name}</h5>
                         <p class="card-text">${product.description}</p>
                         <p class="card-text"><strong>Precio:</strong> $${product.price || 'N/A'}</p>
-                        <p class="card-text"><strong>Stock:</strong> $${product.stock || '0'}</p>
+                        <p class="card-text"><strong>Stock:</strong> ${product.stock || '0'} unidades</p>
                     </div>
                     <div class="card-footer d-flex justify-content-between">
                         <button class="btn btn-success btn-xl">Agregar</button>
