5665f279f57aea9f67d8a504b8391e3a26552d8d
Daniel Vega Coto <c.daniel.vega@gmail.com>
2024-12-19 00:03:21 -0600

    agregar un alert si sale mal

--- js/cart.js
+++ js/cart.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const checkoutButton = document.getElementById("checkout-button");
   let cartId;
 
- 
+
   const renderCart = async () => {
     cartItemsContainer.innerHTML = "";
     const response = await fetch("backend/cart-item.php", {
@@ -30,71 +30,71 @@ document.addEventListener("DOMContentLoaded", async () => {
                 `;
         cartItemsContainer.appendChild(productElement);
 
-            });
-            console.log(cartInfo.items);
-            cartTotalContainer.innerHTML = `<h3>Total: $${cartInfo.items.reduce((accumulator, p) => accumulator + Number(p.product_price), 0)}</h3>`;
-            document.querySelectorAll(".remove-item").forEach(btn => {
-                btn.addEventListener('click', async (e) => {
-                    e.preventDefault();
-                    const id = e.target.dataset.index;
-                    if(!id){
-                        alert("No se pudo borrar el producto, favor refrescar la pagina.");
-                        return;
-                    }
-                    const response = await fetch("http://localhost:3000/backend/cart-item.php", {
-                        credentials: 'include',
-                        method: 'DELETE',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                       // body: { id }
-                       body: JSON.stringify({ id }) // en json se envia el id
-                    });
-                        console.log(response);
-                    if (response.ok) {
-                        await renderCart(); // Recargar el carrito después de eliminar el producto
-                        await   window.refreshCartCount();
-                        renderSuccess("Producto eliminado del carrito.");
+      });
+      console.log(cartInfo.items);
+      cartTotalContainer.innerHTML = `<h3>Total: $${cartInfo.items.reduce((accumulator, p) => accumulator + Number(p.product_price), 0)}</h3>`;
+      document.querySelectorAll(".remove-item").forEach(btn => {
+        btn.addEventListener('click', async (e) => {
+          e.preventDefault();
+          const id = e.target.dataset.index;
+          if (!id) {
+            alert("No se pudo borrar el producto, favor refrescar la pagina.");
+            return;
+          }
+          const response = await fetch("http://localhost:3000/backend/cart-item.php", {
+            credentials: 'include',
+            method: 'DELETE',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            // body: { id }
+            body: JSON.stringify({ id }) // en json se envia el id
+          });
+          console.log(response);
+          if (response.ok) {
+            await renderCart(); // Recargar el carrito después de eliminar el producto
+            await window.refreshCartCount();
+            renderSuccess("Producto eliminado del carrito.");
 
-                    } else {
-                        alert("Error eliminando el producto del carrito.");
-                    }
-
-                   
-                });
-            });
-        } else {
-            // improve alerts
-            alert("oops something went wrong loading your current cart");
-        }
-    };
+          } else {
+            alert("Error eliminando el producto del carrito.");
+          }
 
 
+        });
+      });
+    } else {
+      // improve alerts
+      alert("oops something went wrong loading your current cart");
+    }
+  };
 
-    async function  renderSuccess(message)  {
-        // Selecciona el contenedor donde se mostrará el mensaje
-        const container = document.querySelector('.container');
-    
-        // Crea un elemento `div` con el mensaje
-        const alertDiv = document.createElement('div');
-        alertDiv.className = 'alert alert-danger alert-dismissible fade show';
-        alertDiv.role = 'alert';
-        alertDiv.innerHTML = `
+
+
+  async function renderSuccess(message) {
+    // Selecciona el contenedor donde se mostrará el mensaje
+    const container = document.querySelector('.container');
+
+    // Crea un elemento `div` con el mensaje
+    const alertDiv = document.createElement('div');
+    alertDiv.className = 'alert alert-danger alert-dismissible fade show';
+    alertDiv.role = 'alert';
+    alertDiv.innerHTML = `
             ${message}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         `;
-    
-        // Agrega el mensaje al contenedor
-        container.appendChild(alertDiv);
-     //   await renderCart(); // Recargar el carrito después de eliminar el producto
-     //   await   window.refreshCartCount();
-        // Elimina la notificación automáticamente después de 3 segundos
-        setTimeout(() => {
-            alertDiv.classList.remove('show');
-            alertDiv.classList.add('hide');
-            setTimeout(() => alertDiv.remove(), 500); // Elimina el elemento del DOM
-        }, 3000);
-    }
+
+    // Agrega el mensaje al contenedor
+    container.appendChild(alertDiv);
+    //   await renderCart(); // Recargar el carrito después de eliminar el producto
+    //   await   window.refreshCartCount();
+    // Elimina la notificación automáticamente después de 3 segundos
+    setTimeout(() => {
+      alertDiv.classList.remove('show');
+      alertDiv.classList.add('hide');
+      setTimeout(() => alertDiv.remove(), 500); // Elimina el elemento del DOM
+    }, 3000);
+  }
 
 
   checkoutButton.addEventListener("click", async () => {
@@ -109,7 +109,8 @@ document.addEventListener("DOMContentLoaded", async () => {
       const result = await response.json();
       console.log(result);
     } else {
-
+      // TODO: hacer esto bonito
+      alert("No se pudo proceder con la compra.")
     }
   });
 
